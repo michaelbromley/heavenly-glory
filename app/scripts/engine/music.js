7eb353d6f6a8a8e2990cc3e02ec6e8970c8a517b
Michael Bromley <michael@michaelbromley.co.uk>
2014-12-01 21:04:32 +0100

    Add music fade control

--- app/scripts/engine/music.js
+++ app/scripts/engine/music.js
@@ -7,25 +7,27 @@ var music = (function() {
     var module = {},
         masterGain,
         context,
-        musicLoop;
+        musicLoop,
+        gain = 1;
 
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new AudioContext();
     masterGain = context.createGain();
     masterGain.connect(context.destination);
 
-    module.load = function() {
-            var bufferLoader = new BufferLoader(
-                context,
-                ['assets/audio/music-loop-01.mp3'],
-                finishedLoading
-            );
-
-            bufferLoader.load();
-        };
+    module.load = function(initialVolume) {
+        var bufferLoader = new BufferLoader(
+            context,
+            ['assets/audio/music-loop-01.mp3'],
+            finishedLoading
+        );
+        gain = initialVolume;
+        bufferLoader.load();
+    };
 
     function finishedLoading(bufferList) {
         musicLoop = new Sound(bufferList[0], context, masterGain);
+        musicLoop.setGain(gain);
     }
 
     module.play = function() {
@@ -33,6 +35,28 @@ var music = (function() {
             musicLoop.play(null, true);
         }
     };
+
+    module.setVolume = function(val) {
+        if (0 <= val && val <= 1) {
+            gain = val;
+            musicLoop.setGain(gain);
+        }
+    };
+
+    module.fadeOut = function() {
+        fadeOut();
+    };
+
+    function fadeOut() {
+        var decrement = 0.05;
+        if (decrement <= gain) {
+            gain -= decrement;
+            module.setVolume(gain);
+            setTimeout(fadeOut, 700);
+        } else {
+            musicLoop.stop();
+        }
+    }
 
     return module;
 
